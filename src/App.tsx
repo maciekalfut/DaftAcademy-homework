da14e1350796db97edab4f56375f38df55144a32
Maciej Alfut <57626387+maciekalfut@users.noreply.github.com>
2022-05-31 11:44:42 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, Route, Link} from "react-router-dom";
+import {Routes, Route,} from "react-router-dom";
 import{Navigation} from './navigation'
 import './App.css';
 import {InputForm} from "./InputForm";
