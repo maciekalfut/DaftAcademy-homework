028633e7f6a1c341864f0213b9d34881bc78f5bb
Maciej Alfut <57626387+maciekalfut@users.noreply.github.com>
2022-06-07 13:51:12 +0200

    Add files via upload

---
+++ src/api/ApiPage.tsx
@@ -0,0 +1,8 @@
+export const ApiPage = () => {
+    return <div style={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap'}}>
+        Api
+        <br></br>
+        <ul></ul>
+        <br></br>
+    </div>
+}
